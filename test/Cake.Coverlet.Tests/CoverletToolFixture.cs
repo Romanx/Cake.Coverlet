e4ae9510ae8e24b8c3a3a2f6a57c6b553902536c
Romanx <mcauliffe.alex@gmail.com>
2018-10-23 18:12:08 +0100

    Add test project and basic test

---
+++ test/Cake.Coverlet.Tests/CoverletToolFixture.cs
@@ -0,0 +1,21 @@
+﻿using Cake.Core.IO;
+using Cake.Testing.Fixtures;
+
+namespace Cake.Coverlet.Tests
+{
+    public class CoverletToolFixture : ToolFixture<CoverletSettings>
+    {
+        public CoverletToolFixture() : base("coverlet")
+        {
+        }
+
+        public FilePath TestFile { get; set; }
+        public FilePath TestProject { get; set; }
+
+        protected override void RunTool()
+        {
+            var tool = new CoverletTool(FileSystem, Environment, ProcessRunner, Tools);
+            tool.Run(TestFile, TestProject, Settings);
+        }
+    }
+}
