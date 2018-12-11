a050f2b79f90cd6f710f9780aea95e337ecd3326
David Gardiner <david@gardiner.net.au>
2018-12-11 16:00:51 +1030

    Add TeamCity to output formats
    Implements #15

--- test/Cake.Coverlet.Tests/CoverletToolTest.cs
+++ test/Cake.Coverlet.Tests/CoverletToolTest.cs
@@ -14,7 +14,7 @@ namespace Cake.Coverlet.Tests
         }
 
         [Fact]
-        public void AbsolutePath_Should_Be_Pased()
+        public void AbsolutePath_Should_Be_Passed()
         {
             _fixture.Settings = new CoverletSettings();
             _fixture.TestFile = "./test/Cake.Coverlet.Tests/bin/Debug/netcoreapp2.1/Cake.Coverlet.Tests.dll";
@@ -25,6 +25,21 @@ namespace Cake.Coverlet.Tests
             result.Args.Should().StartWith("\"/Working/test/Cake.Coverlet.Tests/bin/Debug/netcoreapp2.1/Cake.Coverlet.Tests.dll\"");
             result.Args.Should().Contain("--targetargs \"test /Working/test/Cake.Coverlet.Tests --no-build\"");
             result.Args.Should().Contain("--format json");
+        }
+
+        [Fact]
+        public void OutputFormat_Supports_TeamCity()
+        {
+            _fixture.Settings = new CoverletSettings 
+            {
+                CoverletOutputFormat = CoverletOutputFormat.teamcity
+            };
+            _fixture.TestFile = "./test/Cake.Coverlet.Tests/bin/Debug/netcoreapp2.1/Cake.Coverlet.Tests.dll";
+            _fixture.TestProject = "./test/Cake.Coverlet.Tests/";
+
+            var result = _fixture.Run();
+
+            result.Args.Should().Contain("--format teamcity");
         }
     }
 }
